76efbd82ef0b9d8daa03d004936ee2321df7bfc2
gwanghui97 <lgh970409@gmail.com>
2024-10-01 12:36:28 +0900

    feat: #1 KafkaConfig 설정

--- src/main/java/com/pingpong/config/KafkaConfig.java
+++ src/main/java/com/pingpong/config/KafkaConfig.java
@@ -1,11 +1,10 @@
-package com.chat.config;
+package com.pingpong.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
