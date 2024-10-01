68303e74fc0c6025cb3e978095cd20f479a028cc
gwanghui97 <lgh970409@gmail.com>
2024-10-01 15:24:46 +0900

    feat: #2 chat Controller, Service 구현

--- src/main/java/com/pingpong/config/WebSocketConfig.java
+++ src/main/java/com/pingpong/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.chat.config;
+package com.pingpong.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
