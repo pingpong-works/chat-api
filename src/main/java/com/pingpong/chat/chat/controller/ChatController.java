74e87c964673afabb5f2b22767d52bf238492d0e
quokkavely <yellowjerri@gmail.com>
2024-10-17 16:08:24 +0900

    fix #6: cors error

--- src/main/java/com/pingpong/chat/chat/controller/ChatController.java
+++ src/main/java/com/pingpong/chat/chat/controller/ChatController.java
@@ -5,9 +5,11 @@ import com.pingpong.chat.message.dto.ChatMessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RequiredArgsConstructor
 @Controller
+@CrossOrigin(origins = "*")
 public class ChatController {
     private final ChatService charService;
 
