889c8e0b3e1938cc3b9c65dfc82ed3e74b927c8a
quokkavely <yellowjerri@gmail.com>
2024-10-17 16:42:43 +0900

    fix #6 : find my chatroom

--- src/main/java/com/pingpong/chat/room/repository/ChatRoomRepository.java
+++ src/main/java/com/pingpong/chat/room/repository/ChatRoomRepository.java
@@ -2,12 +2,16 @@ package com.pingpong.chat.room.repository;
 
 import com.pingpong.chat.room.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 @EnableJpaRepositories
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     ChatRoom findByChatRoomId(String chatRoomId);
-    List<ChatRoom> findByParticipantsUserId(String userId);
+    //    List<ChatRoom> findByParticipantsUserId(String userId);
+    @Query("SELECT cr FROM ChatRoom cr JOIN cr.participants p WHERE p.userId = :userId")
+    List<ChatRoom> findByParticipantsUserId(@Param("userId") String userId);
 }
